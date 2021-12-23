9810de8369761cc894e123fbfd09f104c6d506ea
Wendy Lee <WLee@DiscoveryEd.com>
2021-12-23 17:33:33 -0600

    initial build of fct_orders and dim_customers

---
+++ dev/compiled/dbt_tutorial/models/staging/stripe/stg_payments.sql
@@ -0,0 +1,17 @@
+with payments as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        created as created_at,
+
+        -- amount is stored in cents, convert to dollars
+        amount / 100 as amount,
+        status
+
+    from dbt-tutorial.stripe.payment
+
+)
+
+select * from payments
